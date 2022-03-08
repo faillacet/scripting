b344966581f28774c3871f53a75a55479e555b8f
faillacet <faillace@chapman.edu>
2022-03-08 13:47:48 -0800

    f

--- ec2_setup.sh
+++ ec2_setup.sh
@@ -7,5 +7,6 @@ cd /wcode
 git clone --branch main https://github.com/faillacet/scripting.git
 cp /wcode/scripting/html/index.html /var/www/html/index.html
 cd /var/www/html/
+sed -i 's/$$DATE_HOLDER *=.*/$$DATE_HOLDER='"$(date "+%y%m%d")"'/' index.html
 service httpd start
 chkconfig httpd on
