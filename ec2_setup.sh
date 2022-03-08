1f44e264ea353252b80bd8101e7f97eca377f14a
faillacet <faillace@chapman.edu>
2022-03-08 13:13:37 -0800

    f

--- ec2_setup.sh
+++ ec2_setup.sh
@@ -5,7 +5,7 @@ yum install git -y
 mkdir /wcode
 cd /wcode
 git clone --branch main https://github.com/faillacet/scripting.git
-cp wcode/scripting/html/index.html /var/www/html/index.html
+cp /wcode/scripting/html/index.html /var/www/html/index.html
 sed -i 's/DATE_REPLACE/${DATE_ENV}/g'
 service httpd start
 chkconfig httpd on
