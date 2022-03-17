c524a0cd46d37f359d44befd5ce7afe83b699692
Trenton Faillace <71678765+faillacet@users.noreply.github.com>
2022-03-16 18:56:23 -0700

    Update ec2_setup.sh

--- ec2_setup.sh
+++ ec2_setup.sh
@@ -7,8 +7,8 @@ cd /test
 git clone --branch main https://github.com/faillacet/scripting.git
 cp /test/scripting/html/index.html /var/www/html/index.html
 cd /var/www/html/
-old="date"
+old="DATE"
 new=$(date +%Y-%m-%d)
-sed -i 's/$date/$new/' index.html
+sed -i 's/$old/$new/' index.html
 service httpd start
 chkconfig httpd on
